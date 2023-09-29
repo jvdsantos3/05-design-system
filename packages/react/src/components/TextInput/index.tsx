ba9fe79f730a75b0bc5abc298be52471a830ce9e
João Vitor <jv.dossantos012@gmail.com>
2023-09-29 02:04:20 -0300

    Multi Step

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from 'react'
+import { Input, Prefix, TextInputContaienr } from './styles'
+
+export interface TextInputProps extends ComponentProps<typeof Input> {
+  prefix?: string
+}
+
+export function TextInput({ prefix, ...props }: TextInputProps) {
+  return (
+    <TextInputContaienr>
+      {!!prefix && <Prefix>{prefix}</Prefix>}
+      <Input {...props} />
+    </TextInputContaienr>
+  )
+}
